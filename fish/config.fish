dc1ea25d170ab7d13dc08cef82c189430e526561
napalm1970 <napalm1970@gmail.com>
2025-07-12 15:16:44 +0300

    Init.

---
+++ fish/config.fish
@@ -0,0 +1,33 @@
+function fish_prompt -d "Write out the prompt"
+    # This shows up as USER@HOST /home/user/ >, with the directory colored
+    # $USER and $hostname are set by fish, so you can just use them
+    # instead of using `whoami` and `hostname`
+    printf '%s@%s %s%s%s > ' $USER $hostname \
+        (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set fish_greeting
+
+end
+
+starship init fish | source
+if test -f ~/.cache/ags/user/generated/terminal/sequences.txt
+    cat ~/.cache/ags/user/generated/terminal/sequences.txt
+end
+
+zoxide init --cmd cd fish | source
+
+alias pamcan=pacman
+alias v=nvim
+alias hh="shutdown -h now"
+alias fc="nvim ~/.config/fish/config.fish"
+alias hc="nvim ~/.config/hypr/hyprland.conf"
+alias wc="nvim ~/.config/waybar/config.jsonc"
+alias cat="bat"
+alias y="yazi"
+alias ls="eza -Ghl --color=always --icons=always"
+
+set -Ux EDITOR nvim
+set fzf_history_time_format %d-%m-%y
