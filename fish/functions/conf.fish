3bc17bb5e472fab7a2a49cb4ba0f2b0c88b05a08
napalm1970 <napalm1970@gmail.com>
2026-01-03 12:22:06 +0200

    Автоматизация: очистка кэша pacman и быстрый редактор конфигов (conf)

---
+++ fish/functions/conf.fish
@@ -0,0 +1,12 @@
+function conf
+    # Папка с конфигами
+    set -l config_dir "$HOME/Documents/config"
+    
+    # Используем fd для поиска файлов (игнорируем .git) и fzf для выбора
+    set -l file (fd --type f --hidden --exclude .git . "$config_dir" | fzf --preview "bat --color=always --style=numbers --line-range=:500 {}")
+
+    # Если файл выбран - открываем его в редакторе
+    if test -n "$file"
+        $EDITOR "$file"
+    end
+end
