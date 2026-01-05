13f897198ffd109aead63424ad0e98db4e17c403
napalm1970 <napalm1970@gmail.com>
2026-01-05 11:06:45 +0200

    Исправление путей: замена ~ на /home/napalm в конфигах Hyprland, Waybar и Fish

--- fish/auto-Hypr.fish
+++ fish/auto-Hypr.fish
@@ -1,5 +1,5 @@
 # Auto start Hyprland on tty1
 if test -z "$DISPLAY" ;and test "$XDG_VTNR" -eq 1
-    mkdir -p ~/.cache
-    exec Hyprland > ~/.cache/hyprland.log ^&1
+    mkdir -p $HOME/.cache
+    exec Hyprland > $HOME/.cache/hyprland.log ^&1
 end
