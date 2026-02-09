132a579ea9fd42bf7149d192eb00838035a4638a
napalm1970 <napalm1970@gmail.com>
2026-02-09 19:33:38 +0200

    chore: Настройка Neovim и Ansible. - Добавлен плагин Avante.nvim для AI. - Удален плагин gemini-cli.nvim. - Обновлен lazy-lock.json. - В Ansible убран флаг -n из become_flags для корректной работы sudo.

---
+++ nvim/lua/plugins/avante.lua
@@ -0,0 +1,62 @@
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  lazy = false,
+  version = false, -- set this if you want to always pull the latest change
+  opts = {
+    -- add any opts here
+    provider = "gemini-cli",
+    auto_suggestions_provider = "gemini-cli",
+    acp_providers = {
+      ["gemini-cli"] = {
+        command = "gemini",
+        args = { "--experimental-acp", "--model", "gemini-3-pro-preview" },
+        env = {
+          NODE_NO_WARNINGS = "1",
+          GEMINI_API_KEY = os.getenv("GEMINI_API_KEY"),
+        },
+      },
+    },
+    providers = {
+      ["gemini-cli"] = {
+        __inherited_from = "gemini",
+        model = "gemini-3-pro-preview",
+      },
+    },
+  },
+  build = "make",
+  dependencies = {
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    --- The below dependencies are optional,
+    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    "zbirenbaum/copilot.lua", -- for providers='copilot'
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+}
