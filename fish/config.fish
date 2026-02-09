50837067780895b1ef0cd1a808de02b24b511c03
napalm1970 <napalm1970@gmail.com>
2026-02-09 08:57:26 +0200

    fix: resolve conflicts and cleanup fish/config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -28,6 +28,7 @@ set fzf_history_time_format %d-%m-%y
 set -g fish_user_paths $HOME/.local/bin
 set -Ux FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
 
+# ESP-IDF / ESP8266 SDK Paths
 fish_add_path $HOME/.local/bin
 
 # Auto-activate System Python Virtual Environment
