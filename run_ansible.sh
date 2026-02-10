0ce71c91744e4d477bb8dab7c1e3a84d03364462
napalm1970 <napalm1970@gmail.com>
2026-02-10 07:36:52 +0200

    Modified run_ansible.sh

--- run_ansible.sh
+++ run_ansible.sh
@@ -2,8 +2,8 @@
 # Запуск настройки системы через Ansible
 
 if ! command -v ansible-playbook &>/dev/null; then
-    echo "Ansible не найден. Установка..."
-    sudo pacman -S --needed --noconfirm ansible
+  echo "Ansible не найден. Установка..."
+  sudo pacman -S --needed --noconfirm ansible
 fi
 
 cd "$(dirname "$0")/ansible" || exit
@@ -11,4 +11,5 @@ cd "$(dirname "$0")/ansible" || exit
 echo "Проверка зависимостей (collections)..."
 ansible-galaxy collection install -r requirements.yml --upgrade
 
-ansible-playbook playbooks/main.yml --ask-become-pass "$@"
+export LC_ALL=C
+ansible-playbook playbooks/main.yml --ask-vault-pass "$@"
