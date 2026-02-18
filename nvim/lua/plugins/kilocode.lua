2df08339c2fd21c882b568c443be1ab3adcf270b
napalm1970 <napalm1970@gmail.com>
2026-02-18 12:08:40 +0200

    feat: обновление Neovim и скриптов плейлистов

--- nvim/lua/plugins/kilocode.lua
+++ nvim/lua/plugins/kilocode.lua
@@ -2,6 +2,15 @@ return -- In your lazy.nvim plugin spec
 {
   "mikaoelitiana/kilo-code.nvim",
   dependencies = { "folke/which-key.nvim" },
+  keys = {
+    {
+      "<leader>kk",
+      function()
+        Snacks.terminal("kilo", { win = { style = "float" } })
+      end,
+      desc = "Kilo Terminal",
+    },
+  },
   config = function()
     require("kilo_code").setup({
       which_key = {
